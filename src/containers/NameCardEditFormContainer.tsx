f4d7bab24c8da1a101f65314f4d8021c0f70779c
Sho Mukai <Sho.Mukai@nab.com.au>
2019-06-15 21:44:50 +1000

    fix: validation for form fields

--- src/containers/NameCardEditFormContainer.tsx
+++ src/containers/NameCardEditFormContainer.tsx
@@ -13,10 +13,10 @@ import { NameCard_Fields } from '../constants';
 export const FormFieldValidator = yup.object({
     firstName: yup.string()
       .required('First name is required.')
-      .matches(/[A-Za-z']+/ , 'Is not in correct format'),
+      .matches(/^[A-Za-z']+$/ , 'Is not in correct format'),
     lastName: yup.string()
       .required('Last name is required.')
-      .matches(/[A-Za-z']+/ , 'Is not in correct format'),
+      .matches(/^[A-Za-z']+$/ , 'Is not in correct format'),
     dateOfBirth: yup.string()
       .required('Date of birth is required.'),
   });
